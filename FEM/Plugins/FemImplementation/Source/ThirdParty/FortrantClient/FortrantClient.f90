a7f07a58cb2230197580b170ab907a4eb42ed020
Mauricio Medina <minimiGamesAM@gmail.com>
2022-05-27 00:19:31 -0400

    Poniendo el mismo codigo del libro en fortran.

--- FEM/Plugins/FemImplementation/Source/ThirdParty/FortrantClient/FortrantClient.f90
+++ FEM/Plugins/FemImplementation/Source/ThirdParty/FortrantClient/FortrantClient.f90
@@ -14,7 +14,66 @@
 
     module mathLibFEM  
         
-    contains      
+    contains
+          
+       SUBROUTINE invert(matrix)
+            !
+            ! This subroutine inverts a small square matrix onto itself.
+            !
+             IMPLICIT NONE
+             INTEGER,PARAMETER::iwp=SELECTED_REAL_KIND(15)
+             REAL(iwp),INTENT(IN OUT)::matrix(:,:)
+             REAL(iwp)::det,j11,j12,j13,j21,j22,j23,j31,j32,j33,con
+             INTEGER::ndim,i,k
+             ndim=UBOUND(matrix,1)
+             IF(ndim==2)THEN
+               det=matrix(1,1)*matrix(2,2)-matrix(1,2)*matrix(2,1)
+               j11=matrix(1,1)
+               matrix(1,1)=matrix(2,2)
+               matrix(2,2)=j11
+               matrix(1,2)=-matrix(1,2)
+               matrix(2,1)=-matrix(2,1)
+               matrix=matrix/det
+             ELSE IF(ndim==3)THEN
+               det=matrix(1,1)*(matrix(2,2)*matrix(3,3)-matrix(3,2)*matrix(2,3))
+               det=det-matrix(1,2)*(matrix(2,1)*matrix(3,3)-matrix(3,1)*matrix(2,3))
+               det=det+matrix(1,3)*(matrix(2,1)*matrix(3,2)-matrix(3,1)*matrix(2,2))
+               j11=matrix(2,2)*matrix(3,3)-matrix(3,2)*matrix(2,3)
+               j21=-matrix(2,1)*matrix(3,3)+matrix(3,1)*matrix(2,3)
+               j31=matrix(2,1)*matrix(3,2)-matrix(3,1)*matrix(2,2)
+               j12=-matrix(1,2)*matrix(3,3)+matrix(3,2)*matrix(1,3)
+               j22=matrix(1,1)*matrix(3,3)-matrix(3,1)*matrix(1,3)
+               j32=-matrix(1,1)*matrix(3,2)+matrix(3,1)*matrix(1,2)
+               j13=matrix(1,2)*matrix(2,3)-matrix(2,2)*matrix(1,3)
+               j23=-matrix(1,1)*matrix(2,3)+matrix(2,1)*matrix(1,3)
+               j33=matrix(1,1)*matrix(2,2)-matrix(2,1)*matrix(1,2)
+               matrix(1,1)=j11
+               matrix(1,2)=j12
+               matrix(1,3)=j13
+               matrix(2,1)=j21
+               matrix(2,2)=j22
+               matrix(2,3)=j23
+               matrix(3,1)=j31
+               matrix(3,2)=j32
+               matrix(3,3)=j33
+               matrix=matrix/det
+             ELSE
+               DO k=1,ndim
+                 con=matrix(k,k)
+                 matrix(k,k)=1.0_iwp
+                 matrix(k,:)=matrix(k,:)/con
+                 DO i=1,ndim
+                   IF(i/=k)THEN
+                     con=matrix(i,k)
+                     matrix(i,k)=0.0_iwp
+                     matrix(i,:)=matrix(i,:)-matrix(k,:)*con
+                   END IF
+                 END DO
+               END DO
+             END IF
+            RETURN
+       END SUBROUTINE invert
+       
        SUBROUTINE beemat(bee, deriv)
             !
             ! This subroutine forms the bee matrix in 2-d (ih=3 or 4) or 3-d (ih=6).
@@ -105,41 +164,79 @@
        
    
     end module mathLibFEM 
+  
+    FUNCTION determinant(jac)RESULT(det)
+           !
+           ! This function returns the determinant of a 1x1, 2x2 or 3x3
+           ! Jacobian matrix.
+           !
+            IMPLICIT NONE    
+            INTEGER,PARAMETER::iwp=SELECTED_REAL_KIND(15)
+            REAL(iwp),INTENT(IN)::jac(:,:)
+            REAL(iwp)::det
+            INTEGER::it 
+            it=UBOUND(jac,1)  
+            SELECT CASE(it)
+            CASE(1)
+              det=1.0_iwp
+            CASE(2)
+              det=jac(1,1)*jac(2,2)-jac(1,2)*jac(2,1)
+            CASE(3)
+              det=jac(1,1)*(jac(2,2)*jac(3,3)-jac(3,2)*jac(2,3))
+              det=det-jac(1,2)*(jac(2,1)*jac(3,3)-jac(3,1)*jac(2,3))
+              det=det+jac(1,3)*(jac(2,1)*jac(3,2)-jac(3,1)*jac(2,2))
+            CASE DEFAULT
+              WRITE(*,*)' wrong dimension for Jacobian matrix'
+            END SELECT
+           RETURN
+    END FUNCTION determinant
     
 program FortrantClient
 
 use mathLibFEM  
 implicit none
-    
-    
+        
     INTEGER, PARAMETER::iwp=SELECTED_REAL_KIND(15)
     
     INTEGER, PARAMETER::dims = 3
     INTEGER, PARAMETER::nbPoints = 4
         
     INTEGER res, i, j
+    REAL(iwp)::det
+    REAL(iwp) determinant ! para llamar la funcion.
+    REAL(iwp) coord(nbPoints, dims)
+    REAL(iwp) der(dims, nbPoints)
+    REAL(iwp) jac(dims, dims)
     REAL(iwp) deriv(dims, nbPoints)
     REAL(iwp) bee(6, nbPoints * 3)
     REAL(iwp) dee(6, 6)
     REAL(iwp) btdb( nbPoints * 3, nbPoints * 3)
     REAL(iwp)::v = 0.3_iwp, e = 10000.0_iwp
-        
-    !REAL ARRAY_B (2:8, -3:20)
+      
+    coord(1, : ) = (/ 0, 0, 1 /)
+    coord(2, : ) = (/ 1, 0, 0 /)
+    coord(3, : ) = (/ 1, 1, 0 /)
+    coord(4, : ) = (/ 0, 0, 0 /)
     
-    !float der[dim * nbPoints] = {
-    !    0, 1, 0, -1,
-    !    0,-1, 1, 0,
-    !    1, 0, 0, -1
-    !};
+    der(1, : ) = (/ 0, 1, 0, -1 /)
+    der(2, : ) = (/ 0,-1, 1, 0  /)
+    der(3, : ) = (/ 1, 0, 0, -1 /)
     
-    deriv(1, : ) = (/ 0, 1, 0, -1 /)
-    deriv(2, : ) = (/ 0,-1, 1, 0 /)
-    deriv(3, : ) = (/ 1, 0, 0, -1 /)
+    jac = MATMUL(der, coord)
     
+    det = determinant(jac)
+    
+    call invert(jac)
+      
+    deriv = MATMUL(jac, der)
+     
     call beemat(bee, deriv)
     call deemat(dee, e, v)
     
     btdb =  MATMUL(MATMUL(TRANSPOSE(bee), dee), bee)
+    
+    !integration
+    btdb = btdb * 1 / 6 * det
     
     do i = LBOUND (btdb, 1), UBOUND (btdb, 1)
       do j = LBOUND (btdb, 2), UBOUND (btdb, 2)
@@ -158,4 +255,3 @@ implicit none
     pause
     
 end program FortrantClient
-
