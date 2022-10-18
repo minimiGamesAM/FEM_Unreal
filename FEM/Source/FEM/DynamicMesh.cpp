1a993390e9d1f0c075c3d0716a599947bb87c010
Mauricio Medina <minimiGamesAM@gmail.com>
2022-10-17 22:06:29 -0400

    Reading vertex colors.

--- FEM/Source/FEM/DynamicMesh.cpp
+++ FEM/Source/FEM/DynamicMesh.cpp
@@ -3,9 +3,12 @@
 
 #include "DynamicMesh.h"
 #include "MeshDescriptionToDynamicMesh.h"
+#include "DynamicMeshToMeshDescription.h"
 #include "StaticMeshAttributes.h"
 #include "FemFunctions.h"
 #include <vector>
+
+#include "Generators/GridBoxMeshGenerator.h"
 
 namespace
 {
@@ -18,22 +21,49 @@ namespace
 		FMeshDescriptionToDynamicMesh converter;
 		converter.Convert(MeshDescription, Mesh);
 	}
+
+	void UpdateStaticMeshFromDynamicMesh(
+		UStaticMesh* StaticMesh,
+		const FDynamicMesh3* Mesh)
+	{
+		FMeshDescription MeshDescription;
+		FStaticMeshAttributes StaticMeshAttributes(MeshDescription);
+		StaticMeshAttributes.Register();
+
+		FDynamicMeshToMeshDescription Converter;
+		Converter.Convert(Mesh, MeshDescription);
+
+		// todo: vertex color support
+
+		//UStaticMesh* StaticMesh = NewObject<UStaticMesh>(Component);
+		//FName MaterialSlotName = StaticMesh->AddMaterial(MyMaterial);
+
+		// Build the static mesh render data, one FMeshDescription* per LOD.
+		TArray<const FMeshDescription*> MeshDescriptionPtrs;
+		MeshDescriptionPtrs.Emplace(&MeshDescription);
+		StaticMesh->BuildFromMeshDescriptions(MeshDescriptionPtrs);
+	}
 }
 // Sets default values
 ADynamicMesh::ADynamicMesh()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void ADynamicMesh::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
+void ADynamicMesh::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	OnMeshGenerationSettingsModified();
+}
+
+////////////////////////////////////////////////////
 void ADynamicMesh::PostLoad()
 {
 	Super::PostLoad();
@@ -45,44 +75,13 @@ void ADynamicMesh::PostActorCreated()
 	Super::PostActorCreated();
 	OnMeshGenerationSettingsModified();
 }
+/////////////////////////////////////////////////////
 
-void ADynamicMesh::EditMesh(TFunctionRef<void(FDynamicMesh3&)> EditFunc)
-{
-	EditFunc(SourceMesh);
-
-	// update spatial data structures
-	//if (bEnableSpatialQueries || bEnableInsideQueries)
-	//{
-	//	MeshAABBTree.Build();
-	//	if (bEnableInsideQueries)
-	//	{
-	//		FastWinding->Build();
-	//	}
-	//}
-
-	OnMeshEditedInternal();
-
-}
 void ADynamicMesh::OnMeshGenerationSettingsModified()
 {
-	EditMesh([this](FDynamicMesh3& MeshToUpdate) {
-		RegenerateSourceMesh(MeshToUpdate);
-		});
-}
-
-void ADynamicMesh::OnMeshEditedInternal()
-{
-	if (MeshComponent == nullptr)
-	{
-		MeshComponent = Cast<UStaticMeshComponent>(GetDefaultSubobjectByName(TEXT("Mesh")));
-
-		if (MeshComponent)
-		{
-			StaticMesh = MeshComponent->GetStaticMesh();
-		}
-	}
-
-	OnMeshModified.Broadcast(this);
+	//EditMesh([this](FDynamicMesh3& MeshToUpdate) {
+		RegenerateSourceMesh(SourceMesh);
+	//	});
 }
 
 void ADynamicMesh::RegenerateSourceMesh(FDynamicMesh3& MeshOut)
@@ -99,20 +98,40 @@ void ADynamicMesh::RegenerateSourceMesh(FDynamicMesh3& MeshOut)
 
 	if (MeshComponent && StaticMesh)
 	{
+		//auto m = &StaticMesh->GetRenderData()->LODResources[0];
+		//auto sec = m->Sections[0];
 
+		TMap<FVector, FColor> VertexColorData;
+		StaticMesh->GetVertexColorData(VertexColorData);
+		
+		//FColorVertexBuffer* colorBuffer = &StaticMesh->GetRenderData()->LODResources[0].VertexBuffers.ColorVertexBuffer;
+		
+		//colorBuffer->Init(colorBuffer->GetNumVertices(), true);
+		//TArray<FColor> colors; 
+		//colorBuffer->GetVertexColors(colors);
+		
 		FMeshDescription* MeshDescription = StaticMesh->GetMeshDescription(0);
-
+		
 		const FVertexArray& VertexIDs = MeshDescription->Vertices();
+		
+		//auto nbVertices = colorBuffer->GetNumVertices();
+
+		//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Pos %i, %i"), nbVertices, VertexIDs.Num()));
+
+		//if (nbVertices == VertexIDs.Num())
+		//{
+
+		//}
 
 		TVertexAttributesConstRef<FVector> VertexPositions =
 			MeshDescription->VertexAttributes().GetAttributesRef<FVector>(MeshAttribute::Vertex::Position);
-
+		
 		float* points = new float[VertexIDs.Num() * 3];
-
+		
 		for (const FVertexID vId : VertexIDs.GetElementIDs())
 		{
 			FVector p = VertexPositions.Get(vId);
-
+		
 			points[vId.GetValue() * 3] = p.X;
 			points[vId.GetValue() * 3 + 1] = p.Y;
 			points[vId.GetValue() * 3 + 2] = p.Z;
@@ -129,9 +148,9 @@ void ADynamicMesh::RegenerateSourceMesh(FDynamicMesh3& MeshOut)
 			faces[triId.GetValue() * 3 + 1] = tri[1].GetValue();
 			faces[triId.GetValue() * 3 + 2] = tri[2].GetValue();
 		}
-
+		
 		////////////////////////////////////////////////////////////////////////
-
+		
 		int		numberOfPoints;
 		double* pointlist;
 		int		numberoftrifaces;
@@ -139,7 +158,7 @@ void ADynamicMesh::RegenerateSourceMesh(FDynamicMesh3& MeshOut)
 		int		numberoftetrahedra;
 		int* tetrahedronlist;
 		int* tet2facelist;
-
+		
 		UFemFunctions::runTetGen(points,
 								 &faces[0],
 								 faces.size(),
@@ -152,13 +171,93 @@ void ADynamicMesh::RegenerateSourceMesh(FDynamicMesh3& MeshOut)
 								 tetrahedronlist,
 								 tet2facelist,
 								 "pqz-f-nn");
+		
+		ProceduralMesh = Cast<UProceduralMeshComponent>(GetDefaultSubobjectByName(TEXT("ProcMesh")));
 
-		MeshOut.Clear();
+		if (ProceduralMesh)
+		{
+			ProceduralMesh->ClearAllMeshSections();
 
+			//ProceduralMesh->CreateMeshSection(0, )
 
+			auto defaultPos = FVector(0.0f, 0.0f, 0.0f);
+			auto defaultColor = FColor(0, 0, 0, 0);
+
+			TArray<FVector>				Vertices(&defaultPos, numberOfPoints);
+			TArray<int32>				Triangles;
+			TArray<FVector>				Normals;
+			TArray<FVector2D>			UVs;
+			TArray<FColor>		        VertexColors(&defaultColor, numberOfPoints);
+			TArray<FProcMeshTangent>	Tangents;
+
+			//MeshOut.Clear();
+
+			//vertex
+			for (int i = 0; i < numberOfPoints; i++)
+			{
+				//MeshOut.AppendVertex(FVector(pointlist[i * 3], pointlist[i * 3] + 1, pointlist[i * 3] + 2));
+
+				FVector p(	pointlist[i * 3 + 0],
+							pointlist[i * 3 + 1],
+							pointlist[i * 3 + 2]);
+
+				UVs.Add(FVector2D(0.0, 0.0));
+				UVs.Add(FVector2D(0.0, 1.0));
+				UVs.Add(FVector2D(1.0, 0.0));
+
+				Vertices[i] = p;
+			}
+			
+			//colors
+			for (int i = 0; i < Vertices.Num(); i++)
+			{
+				FVector& p = Vertices[i];
+
+				FColor* itt = VertexColorData.Find(p);
+
+				if (itt != nullptr)
+				{
+					VertexColors[i] = *itt;
+				}
+			}
+			
+
+			//triangles
+			for (int tetIdx = 0; tetIdx < numberoftetrahedra; tetIdx++)
+			{
+				for (int i = 0; i < 4; ++i)
+				{
+					int idx = tet2facelist[(tetIdx * 4) + i];
+
+					Triangles.Add(trifacelist[idx * 3 + 0]);
+					Triangles.Add(trifacelist[idx * 3 + 1]);
+					Triangles.Add(trifacelist[idx * 3 + 2]);
+
+					//auto tri = FIndex3i(trifacelist[idx * 3 + 0],
+					//	trifacelist[idx * 3 + 1],
+					//	trifacelist[idx * 3 + 2]);
+
+					//MeshOut.AppendTriangle(tri);
+
+					//MeshOut.SetVertexUV(tri[0], FVector2f(0.0, 0.0));
+					//MeshOut.SetVertexUV(tri[1], FVector2f(0.0, 1.0));
+					//MeshOut.SetVertexUV(tri[2], FVector2f(1.0, 0.0));
+				}
+			}
+
+			ProceduralMesh->CreateMeshSection(0, Vertices, Triangles, Normals, UVs, VertexColors, Tangents, false);
+		
+			UMaterialInterface* UseMaterial = UMaterial::GetDefaultMaterial(MD_Surface);
+			
+			if (Material != nullptr)
+			{
+				UseMaterial = Material;
+			}
+
+			ProceduralMesh->SetMaterial(0, UseMaterial);
+		}
 	}
 }
-
 
 // Called every frame
 void ADynamicMesh::Tick(float DeltaTime)
--- FEM/Source/FEM/DynamicMesh.h
+++ FEM/Source/FEM/DynamicMesh.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "DynamicMesh3.h"
 #include "Components/StaticMeshComponent.h"
+#include "ProceduralMeshComponent.h"
 #include "DynamicMesh.generated.h"
 
 UCLASS()
@@ -17,35 +18,46 @@ public:
 	// Sets default values for this actor's properties
 	ADynamicMesh();
 
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(EditAnywhere)
 	UStaticMeshComponent* MeshComponent = nullptr;
 
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(EditAnywhere)
+	UProceduralMeshComponent* ProceduralMesh = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = MaterialOptions)
+	UMaterialInterface* Material = nullptr;
+
+	UPROPERTY(Transient)
 	UStaticMesh* StaticMesh = nullptr;
 
 	/**
 	 * This delegate is broadcast whenever the internal SourceMesh is updated
 	 */
-	DECLARE_MULTICAST_DELEGATE_OneParam(FOnMeshModified, ADynamicMesh*);
-	FOnMeshModified OnMeshModified;
+	//DECLARE_MULTICAST_DELEGATE_OneParam(FOnMeshModified, ADynamicMesh*);
+	//FOnMeshModified OnMeshModified;
 
-	virtual void EditMesh(TFunctionRef<void(FDynamicMesh3&)> EditFunc);
-
-
+	//virtual void EditMesh(TFunctionRef<void(FDynamicMesh3&)> EditFunc);
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	//////////////////////////////////////////////
+	//mutually exclusive
 	virtual void PostLoad() override;
 	virtual void PostActorCreated() override;
+	//////////////////////////////////////////////
 
-	virtual void OnMeshEditedInternal();
-
+	/** Allow actors to initialize themselves on the C++ side after all of their components have been initialized, only called during gameplay */
+	virtual void PostInitializeComponents();
+		
 	/** Called whenever the initial Source mesh needs to be regenerated / re-imported. Calls EditMesh() to do so. */
-	virtual void OnMeshGenerationSettingsModified();
+	//virtual void OnMeshGenerationSettingsModified();
 
 	/** Called to generate or import a new source mesh. Override this to provide your own generated mesh. */
 	virtual void RegenerateSourceMesh(FDynamicMesh3& MeshOut);
+
+	void OnMeshGenerationSettingsModified();
 
 	FDynamicMesh3 SourceMesh;
 
